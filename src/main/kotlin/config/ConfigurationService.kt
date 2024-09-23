f9d879437ef60f3d3648496cbf2b31c493b9aa47
krystianslowik <krystian.slowik@trustedshops.com>
2024-09-23 23:18:16 +0200

    refactor/feat: add support for dynamic configuration via HTTP

--- src/main/kotlin/config/ConfigurationService.kt
+++ src/main/kotlin/config/ConfigurationService.kt
@@ -1,14 +1,16 @@
 package de.jaraco.config
 
 import de.jaraco.model.CustomWireMock
+import de.jaraco.model.StubConfiguration
 import de.jaraco.properties.PropertyLoader
 import de.jaraco.service.StubConfigurationService
-import java.util.Properties
 import java.util.logging.Logger
 
+import java.util.*
+
 class ConfigurationService private constructor(
-  private val propertyLoader: PropertyLoader = PropertyLoader.getInstance(),
-  private val stubConfigurationService: StubConfigurationService = StubConfigurationService.getInstance()
+    private val propertyLoader: PropertyLoader = PropertyLoader.getInstance(),
+    private val stubConfigurationService: StubConfigurationService = StubConfigurationService.getInstance()
 ) {
     companion object {
         private val instance = ConfigurationService()
@@ -16,58 +18,152 @@ class ConfigurationService private constructor(
             return instance
         }
     }
-  private var logger: Logger = Logger.getLogger(this.javaClass.name)
 
+    private var logger: Logger = Logger.getLogger(this.javaClass.name)
 
-  fun configureCustomWireMockBasic(properties: Properties, modus: String): CustomWireMock {
-    val port = propertyLoader.extractPort(properties)
-    logger.info("Configuring CustomWireMock with port: $port")
-    val fileLoc = propertyLoader.extractFileLocation(properties)
-    val customWireMock = CustomWireMock(port.toInt(), fileLoc, modus)
-    customWireMock.createWireMockServer()
-    return customWireMock
-  }
+    private lateinit var customWireMock: CustomWireMock
 
-  fun configureStubs(properties: Properties) {
-    logger.info("Configuring stubs")
-    val hostname = propertyLoader.extractHostname(properties)
-    val port = propertyLoader.extractPort(properties)
-    val requestProperties : Map<String, Properties> = propertyLoader.getRequestConfigurations(properties)
-    for ((key, value) in requestProperties) {
-      logger.info("Request: $key; value: $value")
-      val url = propertyLoader.extractUrl(value)
-      val response = propertyLoader.extractResponse(value)
-      val contentType = propertyLoader.extractContentType(value)
-      val statusCode = propertyLoader.extractStatusCode(value)
-      when (val requestType = propertyLoader.extractRequestType(value)) {
-        "GET" -> {
-          stubConfigurationService.configureGetStub(url, response, contentType, hostname, port.toInt(), statusCode.toInt())}
-        "POST" -> {
-          val requestBody = propertyLoader.extractRequestBody(value)
-          stubConfigurationService.configurePostStub(url, response, contentType, hostname, port.toInt(), requestBody, statusCode.toInt())
-        }
-        "PUT" -> {
-          val requestBody = propertyLoader.extractRequestBody(value)
-          stubConfigurationService.configurePutStub(url, response, contentType, hostname, port.toInt(), statusCode.toInt(), requestBody)
-        }
-        "DELETE" -> stubConfigurationService.configureDeleteStub(url, response, contentType, hostname, port.toInt(), statusCode.toInt())
-        else -> logger.warning("Request type $requestType not supported")
-      }
+    fun configureCustomWireMockBasic(properties: Properties, mode: String): CustomWireMock {
+        val port = propertyLoader.extractPort(properties)
+        logger.info("Configuring CustomWireMock with port: $port")
+        val fileLoc = propertyLoader.extractFileLocation(properties)
+        val customWireMock = CustomWireMock(port.toInt(), fileLoc, mode)
+        customWireMock.createWireMockServer()
+        return customWireMock
     }
-  }
 
-  fun configureCustomWireMock(modus: String, filename: String?) {
-    val properties = propertyLoader.loadPropertyFile(filename, modus)
-    val customWireMock = configureCustomWireMockBasic(properties, modus)
-    customWireMock.startWireMockServer()
-    configureStubs(properties)
-    logger.info("WireMock server started")
-    val isRunning = customWireMock.isWireMockServerRunning()
-    if (isRunning) {
-      logger.info("WireMock server is running")
-    } else {
-      logger.warning("WireMock server is not running")
+    private fun configureStubs(properties: Properties) {
+        logger.info("Configuring stubs")
+        val hostname = propertyLoader.extractHostname(properties)
+        val port = propertyLoader.extractPort(properties)
+        val requestProperties: Map<String, Properties> = propertyLoader.getRequestConfigurations(properties)
+        for ((key, value) in requestProperties) {
+            logger.info("Request: $key; value: $value")
+            val url = propertyLoader.extractUrl(value)
+            val response = propertyLoader.extractResponse(value)
+            val contentType = propertyLoader.extractContentType(value)
+            val statusCode = propertyLoader.extractStatusCode(value)
+            when (val requestType = propertyLoader.extractRequestType(value)) {
+                "GET" -> {
+                    stubConfigurationService.configureGetStub(
+                        url, response, contentType, hostname, port.toInt(), statusCode.toInt()
+                    )
+                }
+
+                "POST" -> {
+                    val requestBody = propertyLoader.extractRequestBody(value)
+                    stubConfigurationService.configurePostStub(
+                        url, response, contentType, hostname, port.toInt(), requestBody, statusCode.toInt()
+                    )
+                }
+
+                "PUT" -> {
+                    val requestBody = propertyLoader.extractRequestBody(value)
+                    stubConfigurationService.configurePutStub(
+                        url, response, contentType, hostname, port.toInt(), statusCode.toInt(), requestBody
+                    )
+                }
+
+                "DELETE" -> stubConfigurationService.configureDeleteStub(
+                    url, response, contentType, hostname, port.toInt(), statusCode.toInt()
+                )
+
+                else -> logger.warning("Request type $requestType not supported")
+            }
+        }
     }
-  }
 
+    fun configureCustomWireMock(mode: String, filename: String?) {
+        val properties = propertyLoader.loadPropertyFile(filename, mode)
+        customWireMock = configureCustomWireMockBasic(
+            properties, mode
+        ) // assigning here, to make sure it's available for ktor config
+        customWireMock.startWireMockServer()
+        configureStubs(properties)
+        logger.info("WireMock server started")
+        val isRunning = customWireMock.isWireMockServerRunning()
+        if (isRunning) {
+            logger.info("WireMock server is running")
+        } else {
+            logger.warning("WireMock server is not running")
+        }
+    }
+
+
+    //incoming stub config from /configure endpoint
+    fun configureStubFromPayload(stubConfig: StubConfiguration) {
+        val port = customWireMock.port
+
+        when (stubConfig.requestType.uppercase()) {
+            "GET" -> {
+                if (stubConfig.responseBody != null) {
+                    stubConfigurationService.configureGetStubKtor(
+                        url = stubConfig.url,
+                        hostname = stubConfig.hostname,
+                        responseBody = stubConfig.responseBody,
+                        contentType = stubConfig.contentType,
+                        port = port,
+                        status = stubConfig.statusCode,
+                    )
+                } else {
+                    logger.warning("No response or responseBody provided for GET request")
+                }
+            }
+
+            "POST" -> {
+                val requestBody = stubConfig.requestBody ?: ".*"
+                if (stubConfig.responseBody != null) {
+                    stubConfigurationService.configurePostStubKtor(
+                        url = stubConfig.url,
+                        hostname = stubConfig.hostname,
+                        responseBody = stubConfig.responseBody,
+                        contentType = stubConfig.contentType,
+                        port = port,
+                        requestBody = requestBody,
+                        status = stubConfig.statusCode,
+                        isRequestBodyRegex = stubConfig.isRequestBodyRegex
+                    )
+                } else {
+                    logger.warning("No response or responseBody provided for POST request")
+                }
+            }
+
+            "PUT" -> {
+                val requestBody = stubConfig.requestBody ?: ".*"
+                if (stubConfig.responseBody != null) {
+                    stubConfigurationService.configurePutStubKtor(
+                        url = stubConfig.url,
+                        hostname = stubConfig.hostname,
+                        responseBody = stubConfig.responseBody,
+                        contentType = stubConfig.contentType,
+                        port = port,
+                        requestBody = requestBody,
+                        status = stubConfig.statusCode,
+                        isRequestBodyRegex = stubConfig.isRequestBodyRegex
+                    )
+                } else {
+                    logger.warning("No response or responseBody provided for PUT request")
+                }
+            }
+
+            "DELETE" -> {
+                if (stubConfig.responseBody != null) {
+                    stubConfigurationService.configureDeleteStubKtor(
+                        url = stubConfig.url,
+                        hostname = stubConfig.hostname,
+                        responseBody = stubConfig.responseBody,
+                        contentType = stubConfig.contentType,
+                        port = port,
+                        status = stubConfig.statusCode,
+                    )
+                } else {
+                    logger.warning("No response or responseBody provided for DELETE request")
+                }
+            }
+
+            else -> {
+                logger.warning("Unsupported request type: ${stubConfig.requestType}")
+            }
+        }
+    }
 }
