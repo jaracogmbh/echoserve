645f5a0bf1f35b671d8e0d0bae9f5ea949313725
krystianslowik <krystian.slowik@trustedshops.com>
2024-09-23 23:14:32 +0200

    refactor: optimize WireMock server creation and reset flow

--- src/main/kotlin/model/CustomWireMock.kt
+++ src/main/kotlin/model/CustomWireMock.kt
@@ -1,43 +1,40 @@
 package de.jaraco.model
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import java.util.logging.Logger
 
 class CustomWireMock(
-  val port: Int,
-  var fileLocation: String,
-  val mode: String
+    val port: Int, var fileLocation: String, val mode: String
 ) {
 
-  private var logger: Logger = Logger.getLogger(this.javaClass.name)
-  lateinit var wireMockServer: WireMockServer;
-  fun startWireMockServer() {
-    this.wireMockServer.start()
-  }
+    private var logger: Logger = Logger.getLogger(this.javaClass.name)
+    private lateinit var wireMockServer: WireMockServer
 
-  fun stopWireMockServer() {
-    wireMockServer.stop()
-  }
-
-  fun resetWireMockServer() {
-    wireMockServer.resetAll()
-  }
-
-  fun isWireMockServerRunning(): Boolean {
-    return wireMockServer.isRunning
-  }
-
-  fun createWireMockServer() {
-    if(mode == "docker") {
-      logger.info("Creating WireMockServer with port: $port and fileLocation: $fileLocation")
-      wireMockServer = WireMockServer(options().port(port).usingFilesUnderDirectory(fileLocation))
+    fun startWireMockServer() {
+        this.wireMockServer.start()
     }
-    else {
-      fileLocation = "src/main/resources"
-      wireMockServer = WireMockServer(options().usingFilesUnderClasspath(fileLocation).port(port))
-    }
-  }
 
+    fun stopWireMockServer() {
+        wireMockServer.stop()
+    }
+
+    fun resetWireMockServer() {
+        wireMockServer.resetAll()
+    }
+
+    fun isWireMockServerRunning(): Boolean {
+        return wireMockServer.isRunning
+    }
+
+    fun createWireMockServer() {
+        if (mode == "docker") {
+            logger.info("Creating WireMockServer with port: $port and fileLocation: $fileLocation")
+            wireMockServer = WireMockServer(options().port(port).usingFilesUnderDirectory(fileLocation))
+        } else {
+            fileLocation = "src/main/resources"
+            logger.info("Creating WireMockServer with port: $port and fileLocation: $fileLocation")
+            wireMockServer = WireMockServer(options().port(port).usingFilesUnderClasspath(fileLocation))
+        }
+    }
 }
