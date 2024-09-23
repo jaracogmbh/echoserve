109acdba9f9f4a00113910a2598b4ec41961a18f
krystianslowik <krystian.slowik@trustedshops.com>
2024-09-23 23:22:12 +0200

    feat: add Ktor server for handling stub configurations via API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,30 +2,56 @@ package de.jaraco
 
 import de.jaraco.config.ConfigurationService
 import de.jaraco.exception.NoConfigDirectoryGivenException
-import java.util.*
+import de.jaraco.model.StubConfiguration
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 import java.util.logging.Logger
 
 fun main(args: Array<String>) {
   val logger: Logger = Logger.getLogger("main")
-  logger.info("test Parameter: ${args.contentToString()}")
-  if(args.isEmpty()){
-    throw IllegalArgumentException("No modus given")
-  }else {
-    val modus = args[0]
+  logger.info("Test Parameters: ${args.contentToString()}")
+
+  if (args.isEmpty()) {
+    throw IllegalArgumentException("No mode given")
+  } else {
+    val mode = args[0]
     val configurationService = ConfigurationService.getInstance()
-    if (modus == "docker") {
+
+    if (mode == "docker") {
       if (args.size == 2) {
         val configFilename = args[1]
-        configurationService.configureCustomWireMock(modus, configFilename)
+        configurationService.configureCustomWireMock(mode = mode, filename = configFilename)
       } else {
         throw NoConfigDirectoryGivenException("No config directory given!")
       }
-    } else if (modus == "local") {
-      configurationService.configureCustomWireMock(modus, null)
-
+    } else if (mode == "local") {
+      configurationService.configureCustomWireMock(mode = mode, filename = null)
     } else {
-      throw IllegalArgumentException("Modus $modus not supported")
+      throw IllegalArgumentException("Mode $mode not supported")
     }
-  }
 
+    // handling /configure endpoint
+    embeddedServer(Netty, port = 19991) { //todo? ser the port in some config?
+      install(ContentNegotiation) {
+        json()
+      }
+      routing {
+        post("/configure") {
+          val stubConfig = call.receive<StubConfiguration>()
+          logger.info("Received configuration: $stubConfig")
+          configurationService.configureStubFromPayload(stubConfig)
+          call.respond(HttpStatusCode.OK, "Stub configured successfully.")
+        }
+      }
+    }.start(wait = true)
+  }
 }
